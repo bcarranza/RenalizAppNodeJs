6eaa2a3897a993ed144e080cc5d8854801b5a23b
bcarranza <bayron.carranza@gmail.com>
2023-08-23 22:14:14 -0600

    develop pipeline

--- .github/scripts/decrypt_secret_dev.sh
+++ .github/scripts/decrypt_secret_dev.sh
@@ -3,4 +3,4 @@
 mkdir $GITHUB_WORKSPACE/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $GITHUB_WORKSPACE/secrets/keyfile.json keyfile.json.gpg
+--output $GITHUB_WORKSPACE/secrets/keyfile_dev.json keyfile_dev.json.gpg
