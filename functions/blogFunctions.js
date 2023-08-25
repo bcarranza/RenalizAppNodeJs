1e83bb11ab872b3c0368cffedf277067a0dc251b
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2023-08-25 16:21:09 -0600

    Test function for blog

---
+++ functions/blogFunctions.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.getAllBlogs = async (request, response) => {
+    try {
+        const db = admin.firestore();
+        const blogCollection = db.collection('Blog');
+        
+        const snapshot = await blogCollection.get();
+        const blogData = [];
+        
+        snapshot.forEach((doc) => {
+            blogData.push(doc.data());
+        });
+        
+        response.status(200).json(blogData);
+    } catch (error) {
+        console.error('Error fetching blog data:', error);
+        response.status(500).send('Internal Server Error');
+    }
+};
