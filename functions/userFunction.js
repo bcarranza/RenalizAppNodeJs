a0ecdf21a213ee1215f520290e831d5611dd9444
RDJ20 <rafael.sandovalg4@gmail.com>
2023-08-26 03:17:26 -0600

    Prueba 1

---
+++ functions/userFunction.js
@@ -0,0 +1,30 @@
+exports.createUser = async (request, response) => {
+    try {
+        const db = admin.firestore();
+        const userCollection = db.collection('Users');
+        
+        const newUser = {
+            loginInfo: {
+                userId: request.body.loginInfo.userId,
+                loginTimestamp: request.body.loginInfo.loginTimestamp,
+                ipAddress: request.body.loginInfo.ipAddress,
+            },
+            googleProfile: {
+                googleId: request.body.googleProfile.googleId,
+                displayName: request.body.googleProfile.displayName,
+                firstName: request.body.googleProfile.firstName,
+                lastName: request.body.googleProfile.lastName,
+                image: request.body.googleProfile.image,
+                email: request.body.googleProfile.email,
+            },
+        };
+        
+        const userRef = await userCollection.add(newUser);
+        const user = await userRef.get();
+        
+        response.status(201).json({ id: user.id, ...user.data() });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        response.status(500).send('Internal Server Error');
+    }
+};
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-exports.http = (request, response) =>{
-    response.status(200).send('Hello World from GCP!');
-};
+const userFunction = require("./functions/userFunction");
+
+exports.createUser = userFunction.createUser
