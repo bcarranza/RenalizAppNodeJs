edb10b7be27eff831a838ec64adc2bae542f1be5
RDJ20 <rafael.sandovalg4@gmail.com>
2023-08-27 03:06:25 -0600

    POST con prueba creado
    Hay una funcion para crear pacientes que tiene una variable de prueba y codigo comentado para implementarlo de manera real cuando sea necesario

--- functions/userFunction.js
+++ functions/userFunction.js
@@ -1,30 +1,37 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const userData = {
+    loginInfo: {
+        userId: "Pedro Fernandes",
+        password: "Otracontra",
+        dateOfBirth: "2000-03-17",
+        loginTimestamp: "2027-08-23T12:34:56Z"
+    },
+    googleProfile: {
+        googleId: "12345656789078901234567890",
+        displayName: "Pedro Fernandes",
+        firstName: "Pedrito",
+        lastName: "Fernandes",
+        image: "https://path-to-image.com/profile.jpg",
+        email: "pedrodoe@gmail.com"
+    }
+};
+
+
 exports.createUser = async (request, response) => {
     try {
         const db = admin.firestore();
         const userCollection = db.collection('Users');
-        
-        const newUser = {
-            loginInfo: {
-                userId: request.body.loginInfo.userId,
-                loginTimestamp: request.body.loginInfo.loginTimestamp,
-                ipAddress: request.body.loginInfo.ipAddress,
-            },
-            googleProfile: {
-                googleId: request.body.googleProfile.googleId,
-                displayName: request.body.googleProfile.displayName,
-                firstName: request.body.googleProfile.firstName,
-                lastName: request.body.googleProfile.lastName,
-                image: request.body.googleProfile.image,
-                email: request.body.googleProfile.email,
-            },
-        };
-        
-        const userRef = await userCollection.add(newUser);
-        const user = await userRef.get();
-        
-        response.status(201).json({ id: user.id, ...user.data() });
+
+        //Descomentar para que funcione sin codigo hardcodeado
+        //const userData = request.body;
+
+        await userCollection.add(userData);
+        response.status(200).send('User created successfully');
+
     } catch (error) {
         console.error('Error creating user:', error);
         response.status(500).send('Internal Server Error');
     }
-};  
+}
