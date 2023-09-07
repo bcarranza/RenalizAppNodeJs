9d5e7b2f623f74eb2f260fae2921dc0c979d0e6c
RDJ20 <69997480+RDJ20@users.noreply.github.com>
2023-09-07 15:01:23 -0600

    Ra 19 api test (#16)
    * GET test y POST resultado creados
    
    * yml arreglado
    
    * Otro cambio de yml
    
    * archivo manejador de firebase creado
    
    * Cambios en el YML
    
    * Correcciones yml
    
    * Para el pull request
    
    * Para el PR
    
    * PR
    
    * id desde el JSON

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,5 +1,4 @@
-const admin = require('firebase-admin');
-admin.initializeApp();
+const admin = require('../database/firebase.js');
 
 exports.getAllBlogs = async (request, response) => {
     try {
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const blogFunctions = require('./functions/blogFunctions')
+const testFunctions = require('./functions/testFunctions')
 
 exports.getAllBlogs = blogFunctions.getAllBlogs
+exports.getTestById = testFunctions.getTests;
+exports.postTestResults = testFunctions.postTestResults;
+
