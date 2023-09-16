59b63ba1e148c8c4a9afbdca5eb834df8ea559c1
ArocheLuis <70065176+larochem20@users.noreply.github.com>
2023-09-16 15:58:53 -0600

    ADD: Upload function

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,26 +1,31 @@
-const admin = require('../database/firebase.js');
+const admin = require("../database/firebase.js");
 
 exports.getAllBlogs = async (req, res) => {
-    try {
-        const db = admin.firestore();
-        
-        const query = db.collection('Blog');
+  try {
+    const db = admin.firestore();
 
-        let page = req.page || 1;
-        let perPage = req.appperPage || 10;
+    const query = db.collection("Blogs");
 
-        const snapshot = await query.limit(perPage).get();
-        
-        const blogData = snapshot.docs.map((doc) => doc.data());
+    let page = req.page || 1;
+    let perPage = req.appperPage || 10;
 
-        console.log(blogData);
-        
-        res.status(200).json({ blogData });
+    const snapshot = await query.orderBy('publication_date', "desc").limit(perPage).get();
 
-    } catch (error) {
-        console.error('Error fetching blog data:', error);
-        response.status(500).send('Internal Server Error');
-    }
+    const blogData = snapshot.docs.map((doc) => doc.data());
+
+    res.status(200).json({ blogData });
+  } catch (error) {
+    console.error("Error fetching blog data:", error);
+    response.status(500).send("Internal Server Error");
+  }
 };
 
+exports.uploadBlogs = async (req, res) => {
+  try {
+    const db = admin.firestore().collection("Blogs");
 
+    await db.add();
+
+      res.status(200).send(("Success"));
+  } catch (error) {}
+};
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const blogFunctions = require('./functions/blogFunctions')
 const testFunctions = require('./functions/testFunctions')
 
-exports.getAllBlogs = blogFunctions.getAllBlogs
+exports.getAllBlogs = blogFunctions.getAllBlogs;
+exports.uploadBlogs = blogFunctions.uploadBlogs;
 exports.getTestById = testFunctions.getTests;
 exports.postTestResults = testFunctions.postTestResults;
 
