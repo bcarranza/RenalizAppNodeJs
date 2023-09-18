470a79fd5017ffe408508187b5ab5b0d96de5e2b
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-18 13:19:39 -0600

    Agregado CORS para blogs

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,20 +1,24 @@
 const admin = require('../database/firebase.js');
+const cors = require('cors'); 
+const corsHandler = cors({ origin: true });
 
 exports.getAllBlogs = async (request, response) => {
-    try {
-        const db = admin.firestore();
-        const blogCollection = db.collection('Blog');
-        
-        const snapshot = await blogCollection.get();
-        const blogData = [];
-        
-        snapshot.forEach((doc) => {
-            blogData.push(doc.data());
-        });
-        
-        response.status(200).json(blogData);
-    } catch (error) {
-        console.error('Error fetching blog data:', error);
-        response.status(500).send('Internal Server Error');
-    }
+    corsHandler(request, response, async () => {
+        try {
+            const db = admin.firestore();
+            const blogCollection = db.collection('Blog');
+            
+            const snapshot = await blogCollection.get();
+            const blogData = [];
+            
+            snapshot.forEach((doc) => {
+                blogData.push(doc.data());
+            });
+            
+            response.status(200).json(blogData);
+        } catch (error) {
+            console.error('Error fetching blog data:', error);
+            response.status(500).send('Internal Server Error');
+        }
+    });
 };
