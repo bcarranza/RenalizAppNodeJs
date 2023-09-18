9ed8d33d46bd866800486a24b011bc25b3124234
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-18 15:37:17 -0600

    Settings de CORS

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,24 +1,32 @@
 const admin = require('../database/firebase.js');
-const cors = require('cors'); 
-const corsHandler = cors({ origin: true });
+const cors = require('cors');
+
+// Configura las opciones CORS
+const corsOptions = {
+  origin: '*', 
+  methods: 'GET, POST', 
+};
+
+// Crea un manejador CORS
+const corsHandler = cors(corsOptions);
 
 exports.getAllBlogs = async (request, response) => {
-    corsHandler(request, response, async () => {
-        try {
-            const db = admin.firestore();
-            const blogCollection = db.collection('Blog');
-            
-            const snapshot = await blogCollection.get();
-            const blogData = [];
-            
-            snapshot.forEach((doc) => {
-                blogData.push(doc.data());
-            });
-            
-            response.status(200).json(blogData);
-        } catch (error) {
-            console.error('Error fetching blog data:', error);
-            response.status(500).send('Internal Server Error');
-        }
-    });
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const blogCollection = db.collection('Blog');
+
+      const snapshot = await blogCollection.get();
+      const blogData = [];
+
+      snapshot.forEach((doc) => {
+        blogData.push(doc.data());
+      });
+
+      response.status(200).json(blogData);
+    } catch (error) {
+      console.error('Error fetching blog data:', error);
+      response.status(500).send('Internal Server Error');
+    }
+  });
 };
