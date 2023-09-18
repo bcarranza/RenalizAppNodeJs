3d44f3d0824707c7f79a51acd79850a211ded405
RDJ20 <69997480+RDJ20@users.noreply.github.com>
2023-09-18 17:35:49 -0600

    Ra 19 api test (#18)
    * GET test y POST resultado creados
    
    * yml arreglado
    
    * Otro cambio de yml
    
    * archivo manejador de firebase creado
    
    * Cambios en el YML
    
    * Correcciones yml
    
    * Para el pull request
    
    * Para el PR
    
    * PR
    
    * id desde el JSON
    
    * Prueba de Cors
    
    * Agregado CORS para blogs
    
    * Settings de CORS
    
    * Otra prueba de CORS
    
    * corregido POST
    
    * Agregado codigo faltante

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,20 +1,32 @@
 const admin = require('../database/firebase.js');
 
 exports.getAllBlogs = async (request, response) => {
-    try {
+   
+    response.set('Access-Control-Allow-Origin', '*');
+
+    if (request.method === 'OPTIONS') {
+        response.set('Access-Control-Allow-Methods', 'POST');
+        response.set('Access-Control-Allow-Headers', 'Content-Type , Authorization');
+        response.set('Access-Control-Max-Age', '3600');
+        response.status(204).send('');
+        return;
+    }
+    else{
+         try {
         const db = admin.firestore();
         const blogCollection = db.collection('Blog');
-        
+            
         const snapshot = await blogCollection.get();
         const blogData = [];
-        
+            
         snapshot.forEach((doc) => {
             blogData.push(doc.data());
         });
-        
+            
         response.status(200).json(blogData);
     } catch (error) {
         console.error('Error fetching blog data:', error);
         response.status(500).send('Internal Server Error');
+    }
     }
 };
