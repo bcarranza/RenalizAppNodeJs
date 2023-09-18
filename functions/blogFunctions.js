571c5c5a01c2f38c1f37884524482a96c7565b87
larochem20 <arocheluis2000@gmail.com>
2023-09-18 15:58:16 -0600

    Added token capture

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -6,14 +6,22 @@ exports.getAllBlogs = async (req, res) => {
 
     const query = db.collection("Blogs");
 
-    let page = req.page || 1;
-    let perPage = req.appperPage || 10;
+    //let page = req.page || 1;
+    let perPage = req.appperPage || 2;
+    let token = req.token || null;
+
+    if (token) {
+      req.startAfter(token);
+    }
 
     const snapshot = await query.orderBy('publication_date', "desc").limit(perPage).get();
 
     const blogData = snapshot.docs.map((doc) => doc.data());
 
-    res.status(200).json({ blogData });
+    const lastBlog = snapshot.docs[snapshot.docs.length - 1];
+    const newToken = lastBlog ? lastBlog.id : null;
+
+    res.status(200).json({ blogData, token: newToken });
   } catch (error) {
     console.error("Error fetching blog data:", error);
     response.status(500).send("Internal Server Error");
