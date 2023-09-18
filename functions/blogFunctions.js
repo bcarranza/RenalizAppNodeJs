54d0d203b0e1de9ad15f94aee749168077d4ecc0
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-18 15:59:10 -0600

    Otra prueba de CORS

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,32 +1,31 @@
 const admin = require('../database/firebase.js');
-const cors = require('cors');
-
-// Configura las opciones CORS
-const corsOptions = {
-  origin: '*', 
-  methods: 'GET, POST', 
-};
-
-// Crea un manejador CORS
-const corsHandler = cors(corsOptions);
 
 exports.getAllBlogs = async (request, response) => {
-  corsHandler(request, response, async () => {
-    try {
-      const db = admin.firestore();
-      const blogCollection = db.collection('Blog');
+   
+    response.set('Access-Control-Allow-Origin', '*');
 
-      const snapshot = await blogCollection.get();
-      const blogData = [];
-
-      snapshot.forEach((doc) => {
-        blogData.push(doc.data());
-      });
-
-      response.status(200).json(blogData);
-    } catch (error) {
-      console.error('Error fetching blog data:', error);
-      response.status(500).send('Internal Server Error');
+    if (request.method === 'OPTIONS') {
+        response.set('Access-Control-Allow-Methods', 'GET');
+        response.set('Access-Control-Allow-Headers', 'Content-Type');
+        response.set('Access-Control-Max-Age', '3600');
+        response.status(204).send('');
+        return;
     }
-  });
+
+    try {
+        const db = admin.firestore();
+        const blogCollection = db.collection('Blog');
+            
+        const snapshot = await blogCollection.get();
+        const blogData = [];
+            
+        snapshot.forEach((doc) => {
+            blogData.push(doc.data());
+        });
+            
+        response.status(200).json(blogData);
+    } catch (error) {
+        console.error('Error fetching blog data:', error);
+        response.status(500).send('Internal Server Error');
+    }
 };
