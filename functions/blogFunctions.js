813c9c248615392a4bc26af62708d02bde354ee0
RDJ20 <rafael.sandovalg4@gmail.com>
2023-09-19 22:45:44 -0600

    Setear CORS en lasa funciones Blog

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,30 +1,50 @@
 const admin = require("../database/firebase.js");
 
-exports.getAllBlogs = async (req, res) => {
-  try {
-    let page = req.page || 1;
-    let perPage = req.perPage || 10;
+const setCorsHeaders = (response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+    if (response.method === 'OPTIONS') {
+        response.set('Access-Control-Allow-Methods', 'POST');
+        response.set('Access-Control-Allow-Headers', 'Content-Type , Authorization');
+        response.set('Access-Control-Max-Age', '3600');
+        response.status(204).send('');
+        return true;
+    }
+    return false;
+};
 
-    const snapshot = await admin
-      .firestore()
-      .collection("Blogs")
-      .orderBy("publication_date", "asc")
-      .offset((page - 1) * perPage)
-      .limit(perPage)
-      .get();
+exports.getAllBlogs = async (request, response) => {
+    if (setCorsHeaders(response)) return;
 
-    res.status(200).json(snapshot.docs.map((doc) => doc.data()));
-  } catch (error) {
-    response.status(500).send("Internal Server Error");
-  }
+    try {
+        let page = request.page || 1;
+        let perPage = request.perPage || 10;
+
+        const snapshot = await admin
+            .firestore()
+            .collection("Blogs")
+            .orderBy("publication_date", "asc")
+            .offset((page - 1) * perPage)
+            .limit(perPage)
+            .get();
+
+        response.status(200).json(snapshot.docs.map((doc) => doc.data()));
+    } catch (error) {
+        console.error('Error fetching blog data:', error);
+        response.status(500).send('Internal Server Error');
+    }
 };
 
 exports.uploadBlogs = async (req, res) => {
-  try {
-    const db = admin.firestore().collection("Blogs");
+    if (setCorsHeaders(res)) return;
 
-    await db.add();
+    try {
+        const db = admin.firestore().collection("Blogs");
 
-    res.status(200).send("Success");
-  } catch (error) {}
+        await db.add();
+
+        res.status(200).send("Success");
+    } catch (error) {
+        console.error('Error uploading blog:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
