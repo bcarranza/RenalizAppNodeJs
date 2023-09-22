de8b76f850143b9205b5145268bc926f1eb76c78
Carlos Vásquez <49454955+lighnio@users.noreply.github.com>
2023-09-22 13:59:49 -0600

    FIX: Params to request (#21)
    Modifies GetAllBlogs endpoint to work with pagination
    * FIX: Params for request
    
    * FIX: manage string parameters
    
    * ADD: Changed to a desc load of data
    
    * ADD: New structure for body response

--- functions/blogFunctions.js
+++ functions/blogFunctions.js
@@ -1,50 +1,62 @@
 const admin = require("../database/firebase.js");
 
 const setCorsHeaders = (response) => {
-    response.set('Access-Control-Allow-Origin', '*');
-    if (response.method === 'OPTIONS') {
-        response.set('Access-Control-Allow-Methods', 'POST');
-        response.set('Access-Control-Allow-Headers', 'Content-Type , Authorization');
-        response.set('Access-Control-Max-Age', '3600');
-        response.status(204).send('');
-        return true;
-    }
-    return false;
+  response.set("Access-Control-Allow-Origin", "*");
+  if (response.method === "OPTIONS") {
+    response.set("Access-Control-Allow-Methods", "POST");
+    response.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type , Authorization"
+    );
+    response.set("Access-Control-Max-Age", "3600");
+    response.status(204).send("");
+    return true;
+  }
+  return false;
 };
 
 exports.getAllBlogs = async (request, response) => {
-    if (setCorsHeaders(response)) return;
+  if (setCorsHeaders(response)) return;
 
-    try {
-        let page = request.page || 1;
-        let perPage = request.perPage || 10;
+  try {
+    let page = parseInt(request.body.page) || 1;
+    let perPage = parseInt(request.body.perPage) || 10;
 
-        const snapshot = await admin
-            .firestore()
-            .collection("Blogs")
-            .orderBy("publication_date", "asc")
-            .offset((page - 1) * perPage)
-            .limit(perPage)
-            .get();
+    const snapshot = await admin
+      .firestore()
+      .collection("Blogs")
+      .orderBy("publication_date", "desc")
+      .offset((page - 1) * perPage)
+      .limit(perPage)
+      .get();
 
-        response.status(200).json(snapshot.docs.map((doc) => doc.data()));
-    } catch (error) {
-        console.error('Error fetching blog data:', error);
-        response.status(500).send('Internal Server Error');
-    }
+    const count = await admin.firestore().collection('Blogs').count().get();
+
+    response
+      .status(200)
+      .json({
+        data: snapshot.docs.map((doc) => doc.data()),
+        page,
+        perPage,
+        allItems: count['_data']['count']
+      });
+  } catch (error) {
+    console.error("Error fetching blog data:", error);
+    response.status(500).send("Internal Server Error");
+  }
 };
 
 exports.uploadBlogs = async (req, res) => {
-    if (setCorsHeaders(res)) return;
+  if (setCorsHeaders(res)) return;
 
-    try {
-        const db = admin.firestore().collection("Blogs");
+  try {
+    const db = admin.firestore().collection("Blogs");
 
-        await db.add();
+    await db.add();
 
-        res.status(200).send("Success");
-    } catch (error) {
-        console.error('Error uploading blog:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    res.status(200).send("Success");
+  } catch (error) {
+    console.error("Error uploading blog:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
