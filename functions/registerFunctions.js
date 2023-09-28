1cc421a23769f9d6ebda07551aa20edc651c6876
anahiJuarez85 <anahijuarez580@gmail.com>
2023-09-27 23:30:48 -0600

    refactorización

--- functions/registerFunctions.js
+++ functions/registerFunctions.js
@@ -10,56 +10,48 @@ exports.postRegister = async (request, response) => {
 
       const resultData = request.body;
 
-      if (!resultData.name ||!resultData.dob || !resultData.phoneNumber) {
-        return response.status(400).json({ message: 'Nombre, fecha de nacimiento y número de teléfono son campos obligatorios' });
-      }
-
-      const { name, dob, address, phoneNumber } = resultData;
-
-      const userData = {
-        name,
-        dob, 
-        address: address || null, 
-        phoneNumber,
-      };
-
-      await RegistroUsuariosCollection.add(resultData);
-
-      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData });
-    } catch (error) {
-      console.error(error);
-      response.status(500).json({ message: 'Error en el registro' });
-    }
-  });
-};/*
-const admin = require('../database/firebase.js');
-const cors = require('cors');
-const corsHandler = cors({ origin: true });
-
-exports.postRegister = async (request, response) => {
-  corsHandler(request, response, async () => {
-    try {
-      const db = admin.firestore();
-      const RegistroUsuariosCollection = db.collection('RegistroUsuarios');
-
-      const resultData = request.body;
-
       // Validaciones para campos obligatorios
-      if (!resultData.name || !resultData.phoneNumber) {
+      if (!resultData.First_Name|| !resultData.Last_Name || !resultData.Birth_Date || !resultData.DPI
+        || !resultData.Telephone_Number || !resultData.Address || !resultData.Department || !resultData.City) {
         return response.status(400).json({ message: 'Nombre y número de teléfono son campos obligatorios' });
       }
 
-      const { uid, name, dob, address, phoneNumber } = resultData;
+      const { uid, First_Name,
+        Second_Name,
+        Last_Name,
+        Second_Last_Name,
+        Birth_Date,
+        DPI,
+        Telephone_Number,
+        Email,
+        Address,
+        Department,
+        City,
+        Blood_Type,
+        Civil_Status,
+        Gender,
+} = resultData;
 
       // Crear un documento en Firestore usando el UID del usuario como identificador
       const userDocRef = RegistroUsuariosCollection.doc(uid);
 
       const userData = {
-        userId: uid, // Guardar el UID en un campo llamado 'userId'
-        name,
-        dob: dob || null,
-        address: address || null,
-        phoneNumber,
+         userId: uid, // Guardar el UID en un campo llamado 'userId'
+         First_Name,
+         Second_Name: Second_Name || null,
+         Last_Name,
+         Second_Last_Name: Second_Last_Name || null,
+         Birth_Date,
+         DPI,
+         Telephone_Number,
+         Email: Email || null,
+         Address,
+         Department,
+         City,
+         Blood_Type: Blood_Type || null,
+         Civil_Status: Civil_Status|| null,
+         Gender: Gender|| null,
+
       };
 
       // Establecer la información del usuario en el documento Firestore
@@ -72,4 +64,3 @@ exports.postRegister = async (request, response) => {
     }
   });
 };
-*/
