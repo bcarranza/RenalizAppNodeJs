22e8d863b6daba72b36be0da85739be1be9fe25c
Zedd2001 <rmendeza6@miumg.edu.gt>
2023-09-28 19:53:53 -0600

    Falta hacer pruebas

--- functions/registerFunctions.js
+++ functions/registerFunctions.js
@@ -1,44 +1,75 @@
-
-const admin = require('../database/firebase.js');//change it to the firebase correct db 
-
-
-const serviceAccount = require('./your-firebase-service-account-key.json'); // replace with your Firebase service account key file
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://your-firebase-project-id.firebaseio.com', // replace with your Firebase project URL
-});
-
-
-// Firebase Authentication
-const auth = admin.auth();
+const admin = require('../database/firebase.js');
+const cors = require('cors');
+const corsHandler = cors({ origin: true });
 
 exports.postRegister = async (request, response) => {
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const RegistroUsuariosCollection = db.collection('RegistroUsuarios');
 
-  try {
-    const { name,  dob, address, phoneNumber } = req.body;
+      const resultData = request.body;
 
-    // Create a new Firebase user
-    const userRecord = await auth.createUser({
-      //email,
-      //password,
-      displayName: name,
-    });
+      if (!resultData.name ||!resultData.dob || !resultData.phoneNumber) {
+        return response.status(400).json({ message: 'Nombre, fecha de nacimiento y número de teléfono son campos obligatorios' });
+      }
 
-    // Send additional user data to Flutter app if needed
-    const userData = {
-      name,
-      //email,
-      dob,
-      address,
-      phoneNumber,
-    };
+      const { name, dob, address, phoneNumber } = resultData;
 
-    res.status(201).json({ message: 'User registered successfully', user: userData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Registration failed' });
-  }
+      const userData = {
+        name,
+        dob, 
+        address: address || null, 
+        phoneNumber,
+      };
+
+      await RegistroUsuariosCollection.add(resultData);
+
+      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: 'Error en el registro' });
+    }
+  });
+};/*
+const admin = require('../database/firebase.js');
+const cors = require('cors');
+const corsHandler = cors({ origin: true });
+
+exports.postRegister = async (request, response) => {
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const RegistroUsuariosCollection = db.collection('RegistroUsuarios');
+
+      const resultData = request.body;
+
+      // Validaciones para campos obligatorios
+      if (!resultData.name || !resultData.phoneNumber) {
+        return response.status(400).json({ message: 'Nombre y número de teléfono son campos obligatorios' });
+      }
+
+      const { uid, name, dob, address, phoneNumber } = resultData;
+
+      // Crear un documento en Firestore usando el UID del usuario como identificador
+      const userDocRef = RegistroUsuariosCollection.doc(uid);
+
+      const userData = {
+        userId: uid, // Guardar el UID en un campo llamado 'userId'
+        name,
+        dob: dob || null,
+        address: address || null,
+        phoneNumber,
+      };
+
+      // Establecer la información del usuario en el documento Firestore
+      await userDocRef.set(userData);
+
+      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: 'Error en el registro' });
+    }
+  });
 };
-
+*/
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const blogFunctions = require('./functions/blogFunctions')
 const testFunctions = require('./functions/testFunctions')
+const registerFunctions = require('./functions/registerFunctions')
 
-exports.getAllBlogs = blogFunctions.getAllBlogs
+exports.getAllBlogs = blogFunctions.getAllBlogs;
+exports.uploadBlogs = blogFunctions.uploadBlogs;
 exports.getTestById = testFunctions.getTests;
 exports.postTestResults = testFunctions.postTestResults;
-
+exports.postRegister = registerFunctions.postRegister;
+exports.getUserByUid = getuserFunctions.getUserByUid;
