a34453be5a8487a5b242263a387d2ea1e4794197
Zedd2001 <60754098+Zedd2001@users.noreply.github.com>
2023-09-29 14:41:42 -0600

    Ra 64 perfil api get (#27)
    * Api sin probar de registro usuarios
    
    * Register Api
    
    * Falta hacer pruebas
    
    * some fixes in serverless.yml
    
    ---------
    
    Co-authored-by: Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const blogFunctions = require('./functions/blogFunctions')
 const testFunctions = require('./functions/testFunctions')
 const registerFunctions = require('./functions/registerFunctions')
+const getuserFunctions = require('./functions/getuserFunctions')
 
 exports.getAllBlogs = blogFunctions.getAllBlogs;
 exports.uploadBlogs = blogFunctions.uploadBlogs;
 exports.getTestById = testFunctions.getTests;
 exports.postTestResults = testFunctions.postTestResults;
 exports.postRegister = registerFunctions.postRegister;
+exports.getUserByUid = getuserFunctions.getUserByUid;
+
