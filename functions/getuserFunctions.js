5cc85fad9d53932c1685c258e44bc92f05fd337e
Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
2023-09-30 01:04:53 -0600

    release v0.2 (#29)
    * Ra 64 perfil api get (#25)
    
    * Api sin probar de registro usuarios
    
    * Register Api
    
    * Falta hacer pruebas
    
    * Revert "Ra 64 perfil api get (#25)" (#26)
    
    This reverts commit 8ac542d0f28dca6c3ae981aaba33924557f729bf.
    
    * Ra 64 perfil api get (#27)
    
    * Api sin probar de registro usuarios
    
    * Register Api
    
    * Falta hacer pruebas
    
    * some fixes in serverless.yml
    
    ---------
    
    Co-authored-by: Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>
    
    * refactorización (#23)
    
    Co-authored-by: anahiJuarez85 <anahijuarez580@gmail.com>
    
    * fix param to body
    
    ---------
    
    Co-authored-by: Zedd2001 <60754098+Zedd2001@users.noreply.github.com>
    Co-authored-by: AnahiJuarez85 <70003358+AnahiJuarez85@users.noreply.github.com>
    Co-authored-by: anahiJuarez85 <anahijuarez580@gmail.com>

---
+++ functions/getuserFunctions.js
@@ -0,0 +1,33 @@
+const admin = require('../database/firebase.js');
+const cors = require('cors');
+const corsHandler = cors({ origin: true });
+
+exports.getUserByUid = async (request, response) => {
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const RegistroUsuariosCollection = db.collection('RegistroUsuarios');
+
+      const { uid } = request.body;
+
+      if (!uid) {
+        return response.status(400).json({ message: 'El UID de usuario es obligatorio' });
+      }
+
+              const userQuery = await RegistroUsuariosCollection.where('userId', '==', uid).get();
+
+      if (userQuery.empty) {
+        return response.status(404).json({ message: `Usuario no encontrado + ${uid}` });
+      }
+
+   
+      const userDoc = userQuery.docs[0];
+      const userData = userDoc.data();
+
+      response.status(200).json({ message: 'Usuario encontrado exitosamente', user: userData });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: 'Error al buscar el usuario' });
+    }
+  });
+};
