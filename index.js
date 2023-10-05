6c6db68329366108ee79b88ca51138b0f199a32e
RDJ20 <69997480+RDJ20@users.noreply.github.com>
2023-10-05 01:57:01 -0600

    API para obtener resultados del test (#31)
    * API para obtener resultados del test
    
    * Cambio yml
    
    * Now is fixed

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const blogFunctions = require('./functions/blogFunctions')
 const testFunctions = require('./functions/testFunctions')
 const registerFunctions = require('./functions/registerFunctions')
 const getuserFunctions = require('./functions/getuserFunctions')
+const getTestsByUid = require('./functions/getTestByUidFunction')
 
 exports.getAllBlogs = blogFunctions.getAllBlogs;
 exports.uploadBlogs = blogFunctions.uploadBlogs;
 exports.getTestById = testFunctions.getTests;
 exports.postTestResults = testFunctions.postTestResults;
+
+exports.getTestsByUid = getTestsByUid.getTestsByUid;
+
+
 exports.postRegister = registerFunctions.postRegister;
 exports.getUserByUid = getuserFunctions.getUserByUid;
 
