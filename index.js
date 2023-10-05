33429a6a93c2cba7a91efc3f5c0199ebbcc9435f
anahiJuarez85 <anahijuarez580@gmail.com>
2023-10-05 17:01:53 -0600

    implementation of the function getMentions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const blogFunctions = require('./functions/blogFunctions')
 const testFunctions = require('./functions/testFunctions')
 const registerFunctions = require('./functions/registerFunctions')
 const getuserFunctions = require('./functions/getuserFunctions')
+const mentionstFunctions = require('./functions/mentionsFunctions')
 
 exports.getAllBlogs = blogFunctions.getAllBlogs;
 exports.uploadBlogs = blogFunctions.uploadBlogs;
@@ -9,4 +10,5 @@ exports.getTestById = testFunctions.getTests;
 exports.postTestResults = testFunctions.postTestResults;
 exports.postRegister = registerFunctions.postRegister;
 exports.getUserByUid = getuserFunctions.getUserByUid;
+exports.getMentions = mentionstFunctions.getMentions;
 
