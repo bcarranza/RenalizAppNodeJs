487f7d52855cd31fdeec0b679cf46022dedd1b0e
Zedd2001 <60754098+Zedd2001@users.noreply.github.com>
2023-10-06 18:18:58 -0600

    Ra 70 api centros asistenciales (#34)
    * Api sin probar de registro usuarios
    
    * Register Api
    
    * Falta hacer pruebas
    
    * Llenar tabla con datos
    
    * fix package-json
    
    * add function by id
    
    * quit message
    
    ---------
    
    Co-authored-by: Danilo Portillo <38937343+DJPORTILLO30@users.noreply.github.com>

---
+++ functions/medcenterFunctions.js
@@ -0,0 +1,55 @@
+const admin = require('../database/firebase.js');
+const cors = require('cors');
+const corsHandler = cors({ origin: true });
+
+exports.getLugaresAtencion = async (request, response) => {
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const LugaresAtencionCollection = db.collection('LugaresAtencion');
+
+      const lugaresQuery = await LugaresAtencionCollection.get();
+
+      if (lugaresQuery.empty) {
+        return response.status(404).json({ message: 'No se encontraron lugares de atención' });
+      }
+      
+      const lugaresData = [];
+      lugaresQuery.forEach((doc) => {
+        lugaresData.push(doc.data());
+      });
+
+      response.status(200).json({
+        lugares: lugaresData,
+      });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: 'Error al buscar lugares de atención' });
+    }
+  });
+};
+
+
+
+exports.getLugaresAtencionbyUID = async (request, response) => {
+  corsHandler(request, response, async () => {
+    try {
+      const db = admin.firestore();
+      const uid = request.body.uid; 
+
+      // Realizar una consulta para obtener un solo documento por su UID
+      const lugarDoc = await db.collection('LugaresAtencion').doc(uid).get();
+
+      if (!lugarDoc.exists) {
+        return response.status(404).json({ message: 'No se encontró el lugar de atención con el UID proporcionado' });
+      }
+
+      response.status(200).json({
+        lugar: lugarDoc.data(),
+      });
+    } catch (error) {
+      console.error(error);
+      response.status(500).json({ message: 'Error al buscar el lugar de atención por UID' });
+    }
+  });
+};
