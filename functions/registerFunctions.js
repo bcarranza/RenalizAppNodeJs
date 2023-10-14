4e63beea52a77d67210eade735198606772d7d2a
Zedd2001 <rmendeza6@miumg.edu.gt>
2023-10-13 18:43:26 -0600

    Email Sender

--- functions/registerFunctions.js
+++ functions/registerFunctions.js
@@ -1,6 +1,7 @@
 const admin = require('../database/firebase.js');
 const cors = require('cors');
 const corsHandler = cors({ origin: true });
+const nodemailer = require('nodemailer'); // Asegúrate de instalar la biblioteca 'nodemailer'
 
 exports.postRegister = async (request, response) => {
   corsHandler(request, response, async () => {
@@ -10,66 +11,50 @@ exports.postRegister = async (request, response) => {
 
       const resultData = request.body;
 
-      if (!resultData.name ||!resultData.dob || !resultData.phoneNumber) {
+      if (!resultData.name || !resultData.dob || !resultData.phoneNumber) {
         return response.status(400).json({ message: 'Nombre, fecha de nacimiento y número de teléfono son campos obligatorios' });
       }
 
-      const { name, dob, address, phoneNumber } = resultData;
+      const { name, dob, address, phoneNumber, email } = resultData;
 
       const userData = {
         name,
-        dob, 
-        address: address || null, 
-        phoneNumber,
-      };
-
-      await RegistroUsuariosCollection.add(resultData);
-
-      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData });
-    } catch (error) {
-      console.error(error);
-      response.status(500).json({ message: 'Error en el registro' });
-    }
-  });
-};/*
-const admin = require('../database/firebase.js');
-const cors = require('cors');
-const corsHandler = cors({ origin: true });
-
-exports.postRegister = async (request, response) => {
-  corsHandler(request, response, async () => {
-    try {
-      const db = admin.firestore();
-      const RegistroUsuariosCollection = db.collection('RegistroUsuarios');
-
-      const resultData = request.body;
-
-      // Validaciones para campos obligatorios
-      if (!resultData.name || !resultData.phoneNumber) {
-        return response.status(400).json({ message: 'Nombre y número de teléfono son campos obligatorios' });
-      }
-
-      const { uid, name, dob, address, phoneNumber } = resultData;
-
-      // Crear un documento en Firestore usando el UID del usuario como identificador
-      const userDocRef = RegistroUsuariosCollection.doc(uid);
-
-      const userData = {
-        userId: uid, // Guardar el UID en un campo llamado 'userId'
-        name,
-        dob: dob || null,
+        dob,
         address: address || null,
         phoneNumber,
       };
 
-      // Establecer la información del usuario en el documento Firestore
-      await userDocRef.set(userData);
+      const userDocRef = await RegistroUsuariosCollection.add(userData);
 
-      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData });
+      // Envía el correo de bienvenida
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'renalizapp@gmail.com', // Cambia esto a tu dirección de correo
+          pass: 'tu_contraseña', // Cambia esto a tu contraseña
+        },
+      });
+
+      const mailOptions = {
+        from: 'renalizapp@gmail.com',
+        to: email, // Usamos el correo proporcionado en la solicitud
+        subject: '¡Bienvenido a RenalizApp!',
+        text: '¡Bienvenido a RenalizApp! Gracias por registrarte en nuestra plataforma.',
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error al enviar el correo de bienvenida:', error);
+        } else {
+          console.log('Correo de bienvenida enviado con éxito:', info.response);
+        }
+      });
+
+      response.status(201).json({ message: 'Usuario registrado exitosamente', user: userData, userId: userDocRef.id });
     } catch (error) {
       console.error(error);
       response.status(500).json({ message: 'Error en el registro' });
     }
   });
 };
-*/
+
